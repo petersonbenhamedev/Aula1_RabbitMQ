07e795da7023517092d6cafbc10ff1ebc13bf4cb
PetersonBenhameCEO <peterson@psicosociall.com.br>
2024-08-18 20:03:55 -0300

    Commit Init

---
+++ Receive/Receive.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+public class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            Port = 5672,
+            Password = "rabbitmq",
+            UserName = "rabbitmq"
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+        Console.WriteLine("[x] Aguardando mensagem");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"[x] Received {message}");
+        };
+
+        channel.BasicConsume(queue: "hello",
+        autoAck: true,
+        consumer: consumer);
+
+        Console.WriteLine("Press [Enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+public class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            Port = 5672,
+            Password = "rabbitmq",
+            UserName = "rabbitmq"
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        const string message = "Olá mundo, vou ser recebido pelo Receive";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: string.Empty,
+                    routingKey: "hello",
+                    basicProperties: null,
+                    body: body);
+
+        Console.WriteLine($"[x] Sent {message}");
+
+        Console.WriteLine("Press [Enter] to exit.");
+        Console.ReadLine();
+    }
+}
